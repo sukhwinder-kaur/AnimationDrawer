f9e22d9bcb4597937d227eef524387e96751282a
Sukhwinder Kaur <sukhwinderkaur624@gmail.com>
2017-07-21 17:16:07 +0530

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,75 @@
+package com.registerlogindemo.activity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.graphics.drawable.DrawerArrowDrawable;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
+
+import com.registerlogindemo.R;
+
+public class MainActivity extends BaseActivity {
+    private static final float END_SCALE = 0.7f;
+
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+    private Toolbar toolbar;
+    private TextView labelView;
+    private View contentView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        navigationView = (NavigationView) findViewById(R.id.navigation_view);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        labelView = (TextView) findViewById(R.id.label);
+        contentView = findViewById(R.id.content);
+
+        toolbar.setNavigationIcon(R.drawable.ic_menu_black_24dp);
+        toolbar.setNavigationIcon(new DrawerArrowDrawable(this));
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+                                                 @Override
+                                                 public void onClick(View v) {
+                                                     if (drawerLayout.isDrawerOpen(navigationView)) {
+                                                         drawerLayout.closeDrawer(navigationView);
+                                                     }
+                                                     else {
+                                                         drawerLayout.openDrawer(navigationView);
+                                                     }
+                                                 }
+                                             }
+        );
+
+        drawerLayout.setScrimColor(Color.TRANSPARENT);
+        drawerLayout.addDrawerListener(new DrawerLayout.SimpleDrawerListener() {
+                                           @Override
+                                           public void onDrawerSlide(View drawerView, float slideOffset) {
+                                               labelView.setVisibility(slideOffset > 0 ? View.VISIBLE : View.GONE);
+
+                                               // Scale the View based on current slide offset
+                                               final float diffScaledOffset = slideOffset * (1 - END_SCALE);
+                                               final float offsetScale = 1 - diffScaledOffset;
+                                               contentView.setScaleX(offsetScale);
+                                               contentView.setScaleY(offsetScale);
+
+                                               // Translate the View, accounting for the scaled width
+                                               final float xOffset = drawerView.getWidth() * slideOffset;
+                                               final float xOffsetDiff = contentView.getWidth() * diffScaledOffset / 2;
+                                               final float xTranslation = xOffset - xOffsetDiff;
+                                               contentView.setTranslationX(xTranslation);
+                                           }
+
+                                           @Override
+                                           public void onDrawerClosed(View drawerView) {
+                                               labelView.setVisibility(View.GONE);
+                                           }
+                                       }
+        );
+    }
+}
